cdd0a9b153dd46d39173cfd50a0fd6c853950407
krishpatel007a-ctrl <krishpatel007a@gmail.com>
2026-06-03 11:34:27 +0530

    done

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -44,7 +44,7 @@ export function AuthProvider({ children }) {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "resumegpt-ai.vercel.app",
+        redirectTo: " http://localhost:5173/",
       },
     });
 
